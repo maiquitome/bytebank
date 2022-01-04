3468d431236837ea3377a1585742198cdbcc336f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-04 06:10:45 -0300

    :sparkles: add custom buttom widget

--- lib/features/dashboard/pages/dashboard_page.dart
+++ lib/features/dashboard/pages/dashboard_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/widgets/custom_buttom.dart';
+
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key key}) : super(key: key);
 
@@ -9,12 +11,21 @@ class DashboardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
-        children: <Widget>[
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Image.asset('assets/images/bytebank_logo.png'),
             ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CustomButtom(),
+            ),
+          ],
+        ),
       ),
     );
   }
