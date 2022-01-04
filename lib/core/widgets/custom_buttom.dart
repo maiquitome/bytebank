a266f187734213b4af7f194a9699f4f62b5bc64c
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-04 06:45:04 -0300

    :sparkles: add GestureDetector

--- lib/core/widgets/custom_buttom.dart
+++ lib/core/widgets/custom_buttom.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 
 class CustomButtom extends StatelessWidget {
-  const CustomButtom({Key key}) : super(key: key);
+  const CustomButtom({Key key, @required this.onTap}) : super(key: key);
+
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      height: 100,
-      width: 150,
-      color: Theme.of(context).primaryColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Icon(
-            Icons.people,
-            color: Colors.white,
-          ),
-          Text(
-            'Contacts',
-            style: TextStyle(color: Colors.white),
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        height: 100,
+        width: 150,
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              Icons.people,
+              color: Colors.white,
+            ),
+            Text(
+              'Contacts',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
