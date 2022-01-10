f02acd10b9663b626f13f8d7b79763c4d83363d0
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-10 20:19:04 -0300

    :card_file_box: add save contact

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,6 +1,8 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../../features/contacts/contact/model/contact_model.dart';
+
 Future<Database> createDatabase() {
   return getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
@@ -11,5 +13,17 @@ Future<Database> createDatabase() {
           'name TEXT, '
           'account_number INTEGER');
     }, version: 1);
+  });
+}
+
+Future<int> save(ContactModel contact) {
+  return createDatabase().then((Database db) {
+    final Map<String, dynamic> contactMap = Map();
+
+    contactMap['id'] = contact.id;
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+
+    return db.insert('contacts', contactMap);
   });
 }
