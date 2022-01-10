198183a1574fb007a5ebbb3948c6dc69b4e1ce12
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-10 20:06:54 -0300

    :sparkles: add id field

--- lib/features/contacts/contact/model/contact_model.dart
+++ lib/features/contacts/contact/model/contact_model.dart
@@ -1,16 +1,18 @@
 class ContactModel {
   ContactModel({
+    this.id,
     this.accountName,
     this.accountNumber,
   });
 
+  int id;
   String accountName;
   int accountNumber;
 
   void setAccountName(String value) {
     accountName = value;
   }
-  
+
   void setAccountNumber(int value) {
     accountNumber = value;
   }
