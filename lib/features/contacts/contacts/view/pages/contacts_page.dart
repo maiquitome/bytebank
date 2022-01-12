42bcd7c0834438aa2a454c0620c4df2909815931
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-12 06:46:34 -0300

    :sparkles: add FutureBuilder

--- lib/features/contacts/contacts/view/pages/contacts_page.dart
+++ lib/features/contacts/contacts/view/pages/contacts_page.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:bytebank/core/database/app_database.dart';
+
 import '../../view/widgets/contact.dart';
 import '../../../contact/model/contact_model.dart';
-import '../../model/contacts_model.dart';
 import '../../../contact/view/pages/contact_page.dart';
 
 class ContactsPage extends StatefulWidget {
   ContactsPage({Key key}) : super(key: key);
-
-  final ContactsModel contactsModel = ContactsModel(
-    contacts: <ContactModel>[
-      ContactModel(name: 'Maiqui Tomé', accountNumber: 74563921)
-    ],
-  );
 
   @override
   _ContactsPageState createState() => _ContactsPageState();
@@ -25,28 +20,29 @@ class _ContactsPageState extends State<ContactsPage> {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView.builder(
-        itemCount: widget.contactsModel.contacts.length,
-        itemBuilder: (BuildContext context, int index) {
-          ContactModel _contactModel = widget.contactsModel.contacts[index];
+      body: FutureBuilder<List<ContactModel>>(
+        initialData: [],
+        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          final List<ContactModel> contacts = snapshot.data;
 
-          return Contact(_contactModel);
+          return ListView.builder(
+            itemCount: contacts.length,
+            itemBuilder: (BuildContext context, int index) {
+              ContactModel _contactModel = contacts[index];
+
+              return Contact(_contactModel);
+            },
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context)
-              .push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) => ContactPage(),
-                ),
-              )
-              .then(
-                (value) => setState(() {
-                  if (value != null)
-                    widget.contactsModel.setContactModel(value);
-                }),
-              );
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) => ContactPage(),
+            ),
+          );
         },
         child: Icon(Icons.add),
       ),
